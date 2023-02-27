03076d914c354639e037c3ab4d9f86afe7625363
shubh8101996 <shubhamshedge8@gmail.com>
2023-02-27 20:55:25 +0530

    commit by shubham

--- src/test/java/abc/Test1.java
+++ src/test/java/abc/Test1.java
@@ -1,12 +1,17 @@
 package abc;
 
 public class Test1 {
-	
-	
+
+	public static void abc() {
+
+		System.out.println("static method");
+
+	}
+
 	public static void main(String[] args) {
-		
+
 		System.out.println("Hiiiii");
-		
+
 	}
 
 }
